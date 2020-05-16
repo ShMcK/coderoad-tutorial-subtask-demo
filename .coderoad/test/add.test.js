8caced06ea67be8ab7cd805ebdfb767aae6b9724
shmck <shawn.j.mckay@gmail.com>
2020-05-16 13:57:19 -0700

    1.1 Add function

---
+++ .coderoad/test/add.test.js
@@ -0,0 +1,27 @@
+// assert docs: https://nodejs.org/api/assert.html#assert_assert
+const assert = require('assert');
+const { add } = require('../../src/add');
+
+describe('SUBTASKS 1.1', function () {
+  it(':1 should add one number', function () {
+    const result = add(1) === 1;
+    const message = 'Should accept a single param';
+    assert.ok(result, message);
+    const result2 = add(42) === 42;
+    assert.ok(result2, message);
+  });
+  it(':2 should add two numbers', function () {
+    const result = add(1, 2) === 3;
+    const message = 'Should accept two params';
+    assert.ok(result, message);
+    const result2 = add(42, 4) === 46;
+    assert.ok(result2, message);
+  });
+  it(':3 should add three numbers', function () {
+    const result = add(1, 2, 3) === 6;
+    const message = 'Should accept three params';
+    assert.ok(result, message);
+    const result2 = add(42, 4, 4) === 50;
+    assert.ok(result2, message);
+  });
+});
